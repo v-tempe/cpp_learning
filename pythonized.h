5b9917777f1e944f9169872d1fb081c8dbe9b241
v-tempe <110841156+v-tempe@users.noreply.github.com>
2023-11-17 19:25:19 +0300

    Create pythonized.h
    A module with some python functions implemented in c++

---
+++ pythonized.h
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+float R = 0;
+
+
+double input(string prompt = "")
+{
+	double value;
+
+	cout << prompt;
+	cin >> value;
+	return value;
+}
+
+
+void print(auto value)
+{
+	cout << value;
+	cout << endl;
+}
+
+
+void debug()
+{
+
+}
+
+
+void input_1(string prompt = "")
+{
+
+}
+
+
+string reversed(string original) {
+    string result = "";
+    for (int i = original.length()-1; i >= 0; i--) result += original[i];
+    return result;
+}
