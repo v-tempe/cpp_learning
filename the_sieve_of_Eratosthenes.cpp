fdc43f3b1a9f4545d008a45b048a5677a1494282
v-tempe <110841156+v-tempe@users.noreply.github.com>
2023-12-21 14:20:27 +0300

    Update the_sieve_of_Eratosthenes.cpp

--- the_sieve_of_Eratosthenes.cpp
+++ the_sieve_of_Eratosthenes.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 
-int primes_on_segment() {
-
-}
-
+int n;
+int amount = 0;
 
 int main()
 {
-    cout<<"Hello World";
+    cin >> n;
+
+    vector<bool> a (n, true);
+    vector<int> b;
+
+
+    for (int j = 2; j < n; j++) {
+        if (a[j] == true) {
+            amount ++;
+            b.push_back(j);
+            for (int i = j + j; i < n; i += j) {
+                a[i] = false;
+            }
+        }
+    }
+
+
+
+    cout << amount << endl;
+    for (int elem : b) cout << elem << ' ';
 
     return 0;
 }
